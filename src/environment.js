b1ff11a9670dedaf5bc196fe6fb0c57e32a1cc93
Amaury <larive.amaury@gmail.com>
2025-06-03 00:38:34 +0200

    .

---
+++ src/environment.js
@@ -0,0 +1,7 @@
+export default {
+  basePath: import.meta.env.VITE_BASE_PATH,
+  emailUser: import.meta.env.VITE_EMAIL_USER,
+  passwordUser: import.meta.env.VITE_PASSWORD_USER,
+  supabaseUrl: import.meta.env.VITE_SUPABASE_URL,
+  supabaseAnonKey: import.meta.env.VITE_SUPABASE_ANON_KEY,
+};
