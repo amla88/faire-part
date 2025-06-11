15e2ec831ecfb0e3923e34cfe1499e8f37f3dd5b
amla88 <144110721+amla88@users.noreply.github.com>
2025-06-11 22:28:55 +0200

    Delete src/env-test.js

--- src/env-test.js
+++
@@ -1,7 +0,0 @@
-export function testEnvVars() {
-  console.log('--- TEST VARIABLES D’ENVIRONNEMENT ---')
-  console.log('VITE_SUPABASE_URL =', import.meta.env.VITE_SUPABASE_URL)
-  console.log('VITE_SUPABASE_ANON_KEY (début) =', import.meta.env.VITE_SUPABASE_ANON_KEY?.slice(0, 10) + '...')
-  console.log('NODE_ENV =', import.meta.env.MODE)
-  console.log('------------------------------------')
-}
